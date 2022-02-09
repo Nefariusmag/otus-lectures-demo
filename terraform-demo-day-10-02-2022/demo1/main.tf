29dc772a8f3c0d6d992a4938ca77ca360a8fde6b
Dmitriy Erokhin <nefariusmag@gmail.com>
2022-02-09 08:53:50 +0300

    Add files for demo day part 1

---
+++ terraform-demo-day-10-02-2022/demo1/main.tf
@@ -0,0 +1,69 @@
+terraform {
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = "0.61.0"
+    }
+  }
+}
+
+provider "yandex" {
+  service_account_key_file = "../../key.json"
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
+  zone                     = var.zone
+}
+
+resource "yandex_compute_instance" "vm-demo" {
+  name                      = "app"
+  allow_stopping_for_update = true
+  scheduling_policy {
+    preemptible = true
+  }
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = var.image_id
+    }
+  }
+
+  network_interface {
+    subnet_id = "${yandex_vpc_subnet.subnet-demo.id}"
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+
+  connection {
+    type        = "ssh"
+    host        = yandex_compute_instance.vm-demo.network_interface.0.nat_ip_address
+    user        = "ubuntu"
+    agent       = false
+    private_key = file("~/.ssh/id_rsa")
+  }
+
+  provisioner "file" {
+    source      = "files/test.txt"
+    destination = "/tmp/test.txt"
+  }
+
+}
+
+resource "yandex_vpc_network" "network-demo" {}
+
+resource "yandex_vpc_subnet" "subnet-demo" {
+  zone           = var.zone
+  network_id     = "${yandex_vpc_network.network-demo.id}"
+  v4_cidr_blocks = [local.ips]
+}
+
+locals {
+  ips = "10.131.0.0/24"
+}
---
+++ terraform-demo-day-10-02-2022/demo1/variables.tf
@@ -0,0 +1,16 @@
+variable "cloud_id" {
+  description = "cloud_id from yandex"
+}
+
+variable "folder_id" {
+  description = "Folder from yandex console"
+}
+
+variable "zone" {
+  default     = "ru-central1-a"
+  description = "Zone"
+}
+
+variable "image_id" {
+  description = "image id that we use"
+}
